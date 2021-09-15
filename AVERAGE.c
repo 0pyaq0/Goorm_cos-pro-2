8897486dd6c4123e34c328736fd99be035d24db8
Sim I Jin <s2011@e-mirim.hs.kr>
2021-09-15 09:08:39 +0900

    Update AVERAGE.c

--- AVERAGE.c
+++ AVERAGE.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-char* solution(char* characters) {
-	char* result = malloc(sizeof(char) * strlen(characters));
-	int result_len = 0;
-	result[0] = characters[0];
-	result_len++;
-	for (int i = 1; i <= strlen(characters); i++) {
-		if (characters[i - 1] != characters[i]) {
-			result[result_len] = characters[i];
-			result_len++;
-		}
-	}
-	result[result_len] = NULL;
-	return result;
+int solution(int data[], int data_len) {
+    double total = 0;
+    for (int i = 0; i < data_len; i++)
+        total += data[i];
+    int cnt = 0;
+    double average = total/data_len;   // double average = data_len / total;
+    for (int i = 0; i < data_len; i++)
+        if (data[i] <= average)
+            cnt += 1;
+    return cnt;
 }
 int main() {
-	char* characters = "senteeeencccccceeee";
-	char* ret = solution(characters);
+    int data1[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int data_len1 = 10;
+    int ret1 = solution(data1, data_len1);
 
-	printf("solution 함수의 반환 값은 %s 입니다.\n", ret);
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
+
+    int data2[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 10 };
+    int data_len2 = 10;
+    int ret2 = solution(data2, data_len2);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
 }
